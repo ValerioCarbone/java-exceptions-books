66803d834cdbf77cd5d792f07589fef38cccc58b
ValerioCarbone <valeriocarbone10@gmail.com>
2024-04-03 16:32:18 +0200

    Add - while & try&catch for n. of pages, try&catch for new book

--- src/org/experis/books/Main.java
+++ src/org/experis/books/Main.java
@@ -1,6 +1,7 @@
 package org.experis.books;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -32,15 +33,25 @@ public class Main {
         for ( int i = 0; i < arrayOfBooksLength; i++ ){
 
         String title,author,editor;
-        int numberOfPages;
+        int numberOfPages = 0;
 
             System.out.println("Insert: Title");
 
             title = scan.nextLine().trim();
 
-            System.out.println("Insert: Number of pages");
+            boolean numberOfPagesIsValid = false;
 
-            numberOfPages = Integer.parseInt(scan.nextLine());
+            while(!numberOfPagesIsValid) {
+                try {
+                    System.out.println("Insert: Number of pages");
+
+                    numberOfPages = Integer.parseInt(scan.nextLine());
+
+                    numberOfPagesIsValid = true;
+                } catch (NumberFormatException e) {
+                    System.out.println("The number of pages is invalid");
+                }
+            }
 
             System.out.println("Insert: Author");
 
@@ -60,10 +71,13 @@ public class Main {
             catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
 
+                System.out.println("The book has not been added");
+
             }
 
 
         }
+        System.out.println(Arrays.toString(catalogue));
 
         scan.close();
 
